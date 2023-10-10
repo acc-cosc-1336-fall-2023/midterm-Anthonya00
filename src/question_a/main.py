52c6c8b3310eed41ce90668a7aec09b6c8c60a14
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2023-10-10 01:51:09 +0000

    Question #a

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,8 @@
 #add import
+
+import question_a
+KM = -32
+MIN = 60
+MPH = question_a.get_miles_per_hour(KM,MIN)
+
+print (MPH)
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,22 @@
 #write functions here, don't add input('') statements here!
 def test_config():
     return True
+
+def get_miles_per_hour(KM,MIN):
+    if KM < 0  or MIN < 0:
+        return "invalid agruments" 
+    else:      
+        miles = KM * .621371
+        hour = MIN/60
+        MPH = miles/hour
+
+        return MPH
+
+
+
+
+
+     
+
+
+
