072b88c66ec536ee3b49a9f8eea9783d342463cc
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2023-10-14 02:40:51 +0000

    Question C

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,1 +1,29 @@
 #add import
+
+import question_c
+
+while True:
+    user_guess = int(input("pick a number between 1 and 5: "))
+    random_number = question_c.get_random_number()
+
+    while user_guess < 1 or user_guess > 5:
+        user_guess = int(input("Your number is not between 1 and 5.pick another number between 1 and 5: "))
+
+    while user_guess != random_number:
+        print("U SUCK, try again. ")
+        user_guess = int(input("pick a number between 1 and 5: "))
+        while user_guess < 1 or user_guess > 5:
+            user_guess = int(input("Your number is not between 1 and 5.pick another number between 1 and 5: "))
+    
+    if user_guess == random_number: 
+        print("congradulations you are correct!!Do you want to try again?: ")
+        Try_again = str(input("select Y to try again: ") )
+        if Try_again == "Y" or Try_again == "y":
+            print ("You selected Try_again")       
+        else: 
+            print ("GAME OVER!!!!")
+
+            break
+   
+    
+        
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -1,18 +1,25 @@
 #write function tests here, don't add input('') statements here!
 import unittest
+
 from src.question_b.question_b import is_prime
+from src.question_c.question_c import get_random_number
 
 #follow this example to add questions b, c, and d for testing including their functions
 from src.question_a.question_a import get_miles_per_hour
 
 
 class Test_Config(unittest.TestCase):
-
+#test case for question A
     def test_question_a_config(self):
         self.assertEqual(get_miles_per_hour(32,60), 19.883872)
-
+#test case for question B
     def test_question_b_config(self):
         self.assertEqual(is_prime(4),False)
         self.assertEqual(is_prime(5),True)
         self.assertEqual(is_prime(11),True)
-
+#test case for question C
+    def test_question_c_config(self): 
+        x = get_random_number()
+        y = x >= 1 and x <= 5
+        self.assertEqual(y,True)
+#test case for question D
